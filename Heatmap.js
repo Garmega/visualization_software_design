6645150de1aa1afa193359d6c4e3aedb84c4b3fa
Garmega <garudaomega@gmail.com>
2016-05-09 04:25:04 -0700

    added charting functions

--- Heatmap.js
+++ Heatmap.js
@@ -6,9 +6,9 @@ window.Heatmap = (function() {
         /*
         user defined data
         */
-        heatmap.xDataLabels = [];
-        heatmap.yDataLabels = [];
-        heatmap.colors = [];
+        heatmap.xDataLabels = ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"];
+        heatmap.yDataLabels = ["1a", "2a", "3a", "4a", "5a", "6a", "7a", "8a", "9a", "10a", "11a", "12a", "1p", "2p", "3p", "4p", "5p", "6p", "7p", "8p", "9p", "10p", "11p", "12p"];
+        heatmap.colors = ["#ffffd9","#edf8b1","#c7e9b4","#7fcdbb","#41b6c4","#1d91c0","#225ea8","#253494","#081d58"];
 
         /*
         User defined dimensions
@@ -29,6 +29,8 @@ window.Heatmap = (function() {
             this.finalCalculations();
 
             //Creates canvas and size accordingly
+            console.log(this.chartIdentifier);
+            // var canvasSvg = d3.selectAll(this.chartIdentifier)
             var canvasSvg = d3.selectAll('#container')
                 .append('svg')
                 .attr('height', this.canvasHeight)
@@ -39,6 +41,54 @@ window.Heatmap = (function() {
                 .attr('transform', 'translate(' + this.margin.left + ',' + this.margin.top + ')')
                 // .attr('height', this.chartHeight)
                 // .attr('width', this.chartWidth);
+
+            var xLabels = chartG.append('g')
+                .data(this.xDataLabels)
+                .enter().append('text')
+                .attr("x", function(d, i) { return i * this.gridSize; })
+                .attr("y", 0)
+                .attr("transform", "translate(" + this.gridSize / 2 + ", -6)")
+                .text(function(d) { return d; })
+                .style("text-anchor", "middle")
+                .attr("class", function(d, i) { return ((i >= 7 && i <= 16) ? "timeLabel mono axis axis-worktime" : "timeLabel mono axis"); });
+
+            var yLabels = chartG.append('g')
+                .data(this.yDataLabels)
+                .enter().append('text')
+                .text(function(d) { return d; })
+                .style('text-anchor', 'middle')
+                .attr('transform', "translate(-6," + this.gridSize / 1.5 + ")")
+                .attr('x', 0)
+                .attr('y', function (d, i) { return i * this.gridSize; })
+                .attr('class', function (d, i) { return ((i >= 0 && i <= 4) ? 'dayLabel mono axis axis-workweek' : 'dayLabel mono axis'); });
+
+            var heatmapChart = function(file) {
+                d3.tsv(file,
+                function(d) {
+                    return {
+                        day: +d.day,
+                        hour: +d.hour,
+                        value: +d.value
+                    };
+                },
+                function(error, data) {
+                    var colorScale = d3.scale.quantile()
+                        .doamin([0, heatmap.colors.length - 1, d3.max(data, function(d) {return d.value;})])
+                        .range(heatmap.colors);
+
+                    var cards = heatmap.chartG.append('rect')
+                        .attr("x", function(d) { return (d.hour - 1) * heatmap.gridSize; })
+                        .attr("y", function(d) { return (d.day - 1) * heatmap.gridSize; })
+                        .attr("rx", 4)
+                        .attr("ry", 4)
+                        .attr("class", "hour bordered")
+                        .attr("width", heatmap.gridSize)
+                        .attr("height", heatmap.gridSize)
+                        .style("fill", heatmap.colors[0]);
+                })
+            }
+
+            heatmapChart('data.tsv')
 
             return this;
         }

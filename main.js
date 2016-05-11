534c49d4ad775c236ee886d84a44d83eacf3e6fb
Garmega <garudaomega@gmail.com>
2016-05-11 03:18:38 -0700

    installed defaults

--- main.js
+++ main.js
@@ -13,10 +13,7 @@ function main() {
         .setFileName('data.tsv')
         .setYLabels(["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"])
         .setXLabels(["1a", "2a", "3a", "4a", "5a", "6a", "7a", "8a", "9a", "10a", "11a", "12a", "1p", "2p", "3p", "4p", "5p", "6p", "7p", "8p", "9p", "10p", "11p", "12p"])
-        .setMargins(50, 50, 50, 50)
         .setDimensions(400, 1200)
-        .setDefaultcolor("#ffffff")
-        .setColors(["#ffffd9","#edf8b1","#c7e9b4","#7fcdbb","#41b6c4","#1d91c0","#225ea8","#253494","#081d58"])
         .build();
 
     var heatmap2 = Heatmap();

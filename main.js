847451f54608a18cdabbe3a15a616d414d851539
Garmega <garudaomega@gmail.com>
2016-05-11 02:40:56 -0700

    plz

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ function main() {
         .setMargins(50, 50, 50, 50)
         .setDimensions(500, 1200)
         .setDefaultcolor("#ffffff")
-        .setColorScale(["#ffffd9","#edf8b1","#c7e9b4","#7fcdbb","#41b6c4","#1d91c0","#225ea8","#253494","#081d58"])
+        .setColors(["#ffffd9","#edf8b1","#c7e9b4","#7fcdbb","#41b6c4","#1d91c0","#225ea8","#253494","#081d58"])
         .build();
 
     console.log(heatmap);

6455a45271abee3b356c1180a796db028af4f037
Garmega <garudaomega@gmail.com>
2016-05-11 02:25:25 -0700

    removed excess yscale code

--- Heatmap.js
+++ Heatmap.js
@@ -42,13 +42,13 @@ window.Heatmap = (function() {
                 // .attr('height', this.chartHeight)
                 // .attr('width', this.chartWidth);
 
-            var yScale = d3.scale.ordinal()
-                .domain(['1', '2', '3', '4', '5', '6', '7'])
-                .rangeBands([0, heatmap.chartHeight]);
-
-            var xScale = d3.scale.ordinal()
-                .domain(['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24'])
-                .rangeBands([0, heatmap.chartWidth])
+            // var yScale = d3.scale.ordinal()
+            //     .domain(['1', '2', '3', '4', '5', '6', '7'])
+            //     .rangeBands([0, heatmap.chartHeight]);
+            //
+            // var xScale = d3.scale.ordinal()
+            //     .domain(['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24'])
+            //     .rangeBands([0, heatmap.chartWidth])
 
             var dayLabels = canvasSvg.selectAll(".dayLabel")
                 .data(heatmap.yDataLabels)

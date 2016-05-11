43a308c53b0c6854131ad7544d61d9593ec5567c
Garmega <garudaomega@gmail.com>
2016-05-11 02:53:54 -0700

    added a second chart

--- main.js
+++ main.js
@@ -16,5 +16,13 @@ function main() {
         .setColors(["#ffffd9","#edf8b1","#c7e9b4","#7fcdbb","#41b6c4","#1d91c0","#225ea8","#253494","#081d58"])
         .build();
 
+    var heatmap2 = Heatmap();
+    heatmap.setIdentifier('container2')
+        .setMargins(100, 100, 100, 100)
+        .setDimensions(250, 600)
+        .setDefaultcolor("#CCCCCC")
+        .setColors(["#C1102B", "#C5BFBC", "#CAF1E7", "#5CC9CB", "#2E707B"])
+        .build();
+
     console.log(heatmap);
 }
